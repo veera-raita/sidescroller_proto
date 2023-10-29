953903f4084320d8b4233a86cbc984b8812231a3
Veera Raita <fvera003@edu.xamk.fi>
2023-10-29 22:00:20 +0200

    woag

--- Assets/Course Library/_Source_Files/Scripts/GameOverer.cs
+++ Assets/Course Library/_Source_Files/Scripts/GameOverer.cs
@@ -17,11 +17,6 @@ public class GameOverer : MonoBehaviour
     public Button RestartButton;
     public Button MenuButton;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
